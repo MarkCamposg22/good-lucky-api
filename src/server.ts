fbefe9077b5b79d17716102151c6704b651f8beb
Markimg22 <marquim.net.221200@hotmail.com>
2024-03-05 16:55:58 -0400

    chore: config server, routes and database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config';
 import express from 'express';
+
+import { bodyParser, contentType, cors, noCache } from './middlewares';
+import { routes } from './routes';
+import { env } from './config';
 
 const server = express();
 
-server.listen(8080, () => {
+server.use(cors);
+server.use(bodyParser);
+server.use(contentType);
+server.use(noCache);
+server.use(routes);
+
+server.listen(env.port, () => {
     console.log('🚀 Server started!');
-    console.log('http://localhost:8080');
+    console.log(`${env.apiUrl}/${env.port}`);
 });
