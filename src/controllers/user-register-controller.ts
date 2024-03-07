7b2d3f2d14bcc6a1c25d2fad8c36f5cf33f40910
GustavoRibas <gustavoribas2018@gmail.com>
2024-03-06 20:22:13 -0400

    criei a rota para login

---
+++ src/controllers/user-register-controller.ts
@@ -0,0 +1,6 @@
+export const userRegisterController = async () => {
+
+    // recebe email, nome, e senha
+    // chama o service user-register-services
+
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,19 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import { UserLoginController } from './controllers/user-login-controller';
 
+const app = express();
 const routes = Router();
 
-// TODO: criar rotas
+// Rota para a página de login
+routes.get('/login', UserLoginController.login);
 
-export { routes };
+// Prefixa todas as rotas com '/api'
+app.use('/api', routes);
+
+// Inicie o servidor na porta 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export { routes }
