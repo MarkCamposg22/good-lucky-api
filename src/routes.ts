9f0c2e7cf4c6b2afc109c7f49bd4eb75dafc1240
jorgeecamps <128872718+jorgeecamps@users.noreply.github.com>
2024-03-08 17:44:13 -0400

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { UserLoginController } from './controllers/UserLoginController';
+import { UserRegisterController } from './controllers/UserRegisterController';
 
 const routes = Router();
 
 // Rota para o login
 routes.post('/login', UserLoginController.login);
 
-export { routes };
+// Rota para o registro de usuário
+routes.post('/register', UserRegisterController.register);
 
+export { routes };
 
