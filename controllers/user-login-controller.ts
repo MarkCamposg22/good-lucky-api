b23f6244fbf850fa1507cb4728ce1231be6c9603
jorgeecamps <128872718+jorgeecamps@users.noreply.github.com>
2024-03-08 16:47:05 -0400

    Delete controllers directory

--- controllers/user-login-controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-export class UserLoginController {
-    public static login(req: Request, res: Response): void {
-
-        res.send('Página de login');
-    }
-}
--- controllers/user-register-controller.ts
+++
@@ -1,5 +0,0 @@
-export const userRegisterController = async (email: string, nome: string, senha: string) => {
-    // Chamada ao serviço de registro de usuário
-    const resultado = await userRegisterServices.registrarUsuario(email, nome, senha);
-    return resultado;
-};
